ac4d9dc562fc9e0b95ffa63d0b641600529999b4
Morgenszter <cezary.sron@gmail.com>
2026-07-11 13:22:14 +0200

    Wire HYDRA HUD services

---
+++ desktop/wpf/src/Hydra.Desktop.Hud/App.xaml.cs
@@ -0,0 +1,37 @@
+using System.Windows;
+using Hydra.Desktop.Hud.Services;
+using Hydra.Desktop.Hud.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Hydra.Desktop.Hud;
+
+public partial class App : Application
+{
+    private readonly IHost _host = Host.CreateDefaultBuilder()
+        .ConfigureServices(services =>
+        {
+            services.AddHttpClient<HydraRuntimeClient>(client =>
+            {
+                client.BaseAddress = new Uri("http://127.0.0.1:5075");
+                client.Timeout = TimeSpan.FromSeconds(3);
+            });
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<MainWindow>();
+        })
+        .Build();
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await _host.StartAsync();
+        _host.Services.GetRequiredService<MainWindow>().Show();
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await _host.StopAsync();
+        _host.Dispose();
+        base.OnExit(e);
+    }
+}
